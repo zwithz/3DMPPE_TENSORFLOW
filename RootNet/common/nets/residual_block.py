c8853bb354d9ef2a042fb0d0bf2499c4e35e304f
Zack Zhou <zwithz1998@gmail.com>
2021-03-15 14:08:21 +0800

    📦 NEW: Add `resnet.py` backbone.

---
+++ RootNet/common/nets/residual_block.py
@@ -0,0 +1,97 @@
+import tensorflow as tf
+from tensorflow.keras.initializers import random_normal
+
+BASE_WEIGHTS_PATH = (
+    'https://storage.googleapis.com/tensorflow/keras-applications/resnet/')
+WEIGHTS_HASHES = {
+    'resnet50v2': ('3ef43a0b657b3be2300d5770ece849e0',
+                   'fac2f116257151a9d068a22e544a4917'),
+    'resnet101v2': ('6343647c601c52e1368623803854d971',
+                    'c0ed64b8031c3730f411d2eb4eea35b5'),
+    'resnet152v2': ('a49b44d1979771252814e80f8ec446f9',
+                    'ed17cf2e0169df9d443503ef94b23b33'),
+}
+
+
+class BasicBlock(tf.keras.layers.Layer):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(out_channels, 3, stride, padding="same", use_bias=False,
+                                            kernel_initializer=random_normal(mean=0, stddev=0.001))
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.conv2 = tf.keras.layers.Conv2D(out_channels, 3, 1, padding="same", use_bias=False,
+                                            kernel_initializer=random_normal(mean=0, stddev=0.001))
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        if stride != 1 or in_channels != self.expansion * out_channels:
+            self.downsample = tf.keras.Sequential()
+            self.downsample.add(tf.keras.layers.Conv2D(self.expansion * out_channels, 1, stride, use_bias=False,
+                                                       kernel_initializer=random_normal(mean=0, stddev=0.001)))
+            self.downsample.add(tf.keras.layers.BatchNormalization())
+        else:
+            self.downsample = None
+
+    def call(self, inputs, training=None, **kwargs):
+        identity = inputs
+
+        x = self.conv1(inputs)
+        x = self.bn1(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x, training=training)
+
+        if self.downsample is not None:
+            identity = self.downsample(inputs)
+
+        x += identity
+        x = tf.nn.relu(x)
+
+        return x
+
+
+class Bottleneck(tf.keras.layers.Layer):
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(out_channels, 1, 1, padding="same", use_bias=False,
+                                            kernel_initializer=random_normal(mean=0, stddev=0.001))
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.conv2 = tf.keras.layers.Conv2D(out_channels, 3, stride, padding="same", use_bias=False,
+                                            kernel_initializer=random_normal(mean=0, stddev=0.001))
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.conv3 = tf.keras.layers.Conv2D(out_channels * self.expansion, 1, 1, padding="same", use_bias=False,
+                                            kernel_initializer=random_normal(mean=0, stddev=0.001))
+        self.bn3 = tf.keras.layers.BatchNormalization()
+        if stride != 1 or in_channels != out_channels * self.expansion:
+            self.downsample = tf.keras.Sequential()
+            self.downsample.add(
+                tf.keras.layers.Conv2D(out_channels * self.expansion, 1, stride, use_bias=False,
+                                       kernel_initializer=random_normal(mean=0, stddev=0.001)))
+            self.downsample.add(tf.keras.layers.BatchNormalization())
+        else:
+            self.downsample = None
+
+    def call(self, inputs, training=None, **kwargs):
+        identity = inputs
+
+        x = self.conv1(inputs)
+        x = self.bn1(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x, training=training)
+
+        if self.downsample is not None:
+            identity = self.downsample(inputs)
+
+        x += identity
+        x = tf.nn.relu(x)
+
+        return x
---
+++ RootNet/common/nets/resnet.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+from tensorflow.keras.initializers import random_normal
+from tensorflow.python.keras.utils import data_utils
+
+from residual_block import Bottleneck, WEIGHTS_HASHES, BASE_WEIGHTS_PATH
+
+
+class ResNetBackbone(tf.keras.Model):
+    def __init__(self, resnet_type):
+        resnet_spec = {50: (Bottleneck, [3, 4, 6, 3], [64, 256, 512, 1024, 2048], 'resnet50v2'),
+                       101: (Bottleneck, [3, 4, 23, 3], [64, 256, 512, 1024, 2048], 'resnet101v2'),
+                       152: (Bottleneck, [3, 8, 36, 3], [64, 256, 512, 1024, 2048], 'resnet152v2')}
+
+        block, layers, channels, name = resnet_spec[resnet_type]
+
+        self.name = name
+        self.inplanes = 64
+
+        super(ResNetBackbone, self).__init__()
+
+        self.conv1 = tf.keras.layers.Conv2D(64, input_shape=3, kernel_size=7, strides=2, padding="same",
+                                            use_bias=False, kernel_initializer=random_normal(mean=0, stddev=0.001))
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.maxpool = tf.keras.layers.MaxPooling2D(pool_size=3, strides=2, padding="same")
+        self.layer1 = self._make_layer(block, 64, blocks=layers[0])
+        self.layer2 = self._make_layer(block, 128, blocks=layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, blocks=layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, blocks=layers[3], stride=2)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        layers = tf.keras.Sequential()
+        layers.add(block(self.inplanes, planes, stride))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.add(block(self.inplanes, planes))
+
+        return layers
+
+    def call(self, x, training=False):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = tf.nn.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x, training=training)
+        x = self.layer2(x, training=training)
+        x = self.layer3(x, training=training)
+        x = self.layer4(x, training=training)
+        return x
+
+    def init_weights(self):
+        file_name = self.name + '_weights_tf_dim_ordering_tf_kernels_notop.h5'
+        file_hash = WEIGHTS_HASHES[self.name][1]
+        weights_path = data_utils.get_file(
+            file_name,
+            BASE_WEIGHTS_PATH + file_name,
+            cache_subdir='models',
+            file_hash=file_hash)
+        self.load_weights(weights_path)
+        print("Initialize resnet from model zoo")
